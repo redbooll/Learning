ba0218543019583ce69e69341b23d33878a3c84a
redbooll <marius.cristian.rosu@gmail.com>
2020-06-22 22:18:34 +0100

    no message

--- react-redux-asp.net/groupKO/groupKo/groupKo/Models/groupDB.cs
+++ react-redux-asp.net/groupKO/groupKo/groupKo/Models/groupDB.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace groupKo.Models
@@ -9,10 +11,13 @@ namespace groupKo.Models
     public class groupDB
     {
         [Key]
+        [Column(Group = "nvarchar(100)")]
         public string TeamName { get; set; }
         
+        [Column(Group = "nvarchar(100)")]
         public int GD { get; set; }
 
+        [Column(Group = "nvarchar(100)")]
         public int PTS { get; set; }
     }
 }
--- react-redux-asp.net/groupKO/groupKo/groupKo/Models/teamDb.cs
+++ react-redux-asp.net/groupKO/groupKo/groupKo/Models/teamDb.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,8 @@ namespace groupKo.Models
 {
     public class teamDb
     {
+        [Key]
+        [Column(RegisteredTeam = "nvarchar(100)")]
+        public string TeamName { get; set; }
     }
 }
